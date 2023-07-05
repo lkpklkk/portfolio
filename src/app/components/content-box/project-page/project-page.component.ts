5f257c7469f0e2886be9a748c9b5f4e0bcd6962b
lkpklkk <lkpklkk.le@mail.utoronto.ca>
2023-07-05 17:35:32 -0400

    update project description

--- src/app/components/content-box/project-page/project-page.component.ts
+++ src/app/components/content-box/project-page/project-page.component.ts
@@ -45,9 +45,9 @@ export class ProjectPageComponent {
       new Project(
         'Portfolio Site',
         'Front End Dev',
-        "This project is my creative take on a lively portfolio website, with an equal focus on style and substance. The site is built on the <span class='highlight'>AngularJS</span> framework, creating a highly modular, customizable and robust application that's all set for any future updates or maintenance.\
-        <br>I've brought the site to life using <span class='highlight'>ThreeJS</span> and <span class='highlight'>AnimeJS</span> libraries, which are the driving forces behind most of the 3D interactions and animations you'll see on the site.\
-        \n In a nutshell, this project isn't just a fun experiment—it's also a practical portfolio website that's ready to showcase my past work and any cool stuff I'll do in the future.",
+        "This project is my creative take on a lively portfolio website, with an equal focus on style and substance. The site is built on the <span class='highlight'>AngularJS</span> framework, creating a highly modular, customizable and robust application that's all set for any future updates or maintenance.<br><br>\
+        <br>I've brought the site to life using <span class='highlight'>ThreeJS</span> and <span class='highlight'>AnimeJS</span> libraries, which are the driving forces behind most of the 3D interactions and animations you'll see on the site.<br><br>\
+         In a nutshell, this project isn't just a fun experiment—it's also a practical portfolio website that's ready to showcase my past work and any cool stuff I'll do in the future.",
         'assets/graphics/smileyBlack.png',
         'assets/imgs/PortfolioSite.jpg',
         '',
@@ -63,7 +63,9 @@ export class ProjectPageComponent {
       new Project(
         'CounterAds',
         'Full Stack Dev',
-        "Using <span class='highlight'>AngularJS</span>, advertisers can showcase their ads, and gamers can try to shoot them down. If they miss, they watch the ad. Incorporating <span class='highlight'>Auth0</span> for security, <span class='highlight'>Socket.IO</span> for real-time play, <span class='highlight'>PostgreSQL</span> for data storage",
+        "The project privides a gamified ad inteface that binds users and advertisers.The application, built using the robust <span class='highlight'>AngularJS</span> framework, introduces an engaging way for advertisers to display their ads and for gamers to interact with them - shoot it or view it!<br><br>\
+        For secure user experiences, <span class='highlight'>Auth0</span> has been seamlessly integrated. The real-time gaming element is brought to life using the powerful <span class='highlight'>Socket.IO</span> library, enabling instantaneous actions and reactions within the game.<br><br>\
+        Data is stored securely and efficiently with <span class='highlight'>PostgreSQL</span>, providing a reliable backend support. This project isn't just a playful blend of gaming and advertising—it also showcases my ability to craft unique, interactive applications that bridge functionality with fun, now and into the future.",
         'assets/graphics/stack.svg',
         'assets/imgs/CounterAds.jpg',
         '',
